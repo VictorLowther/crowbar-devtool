b3368a8b1479dd52af1e56a4217d14c4fe215835
Victor Lowther <victor.lowther@gmail.com>
2013-05-20 11:23:35 -0500

    Flesh out flat metadata model.

--- devtool/flat_metadata.go
+++ devtool/flat_metadata.go
@@ -1,185 +1,218 @@
 package devtool
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
-	"io/ioutil"
-	"fmt"
-	"bytes"
 )
 
-// Holds important information for flat metdata
 type FlatMetadata struct {
-	path string
+	path     string
 	releases ReleaseMap
-	crowbar *Crowbar
+	crowbar  *Crowbar
 }
 
 type FlatRelease struct {
-	name,parent string
-	meta *FlatMetadata
-	builds BuildMap
+	name, parent string
+	meta         *FlatMetadata
+	builds       BuildMap
 }
 
-func (r *FlatRelease) path() (string) {
-	return filepath.Join(r.meta.path,r.name)
+// How to find the pointer to where the on-disk metadata for this release lives.
+func (r *FlatRelease) path() string {
+	return filepath.Join(r.meta.path, r.name)
 }
 
-func (r *FlatRelease) Name() (string) {
+// Fetch the name of a release
+func (r *FlatRelease) Name() string {
 	return r.name
 }
 
-func (r *FlatRelease) Builds() (BuildMap,error) {
+// Get a map of builds for a specific release.
+// They will be indexed in the returned BuildMap y build.Name()
+func (r *FlatRelease) Builds() BuildMap {
 	if r.builds == nil {
-		return nil,fmt.Errorf("Release %s has no builds.",r.name)
+		log.Panicf("Release %s has no builds.", r.name)
 	}
-	return r.builds,nil
+	return r.builds
 }
 
-func (r *FlatRelease) Parent() (Release,error) {
+// Find the parent release of this release.
+// If there isn't one, return nil.
+func (r *FlatRelease) Parent() Release {
 	if r.parent == "" {
-		return nil,nil
+		return nil
 	}
 	if r.meta.releases[r.parent] == nil {
-		return nil,fmt.Errorf("Parent release %s of %s does not exist!",r.parent,r.name)
+		log.Panicf("Parent release %s of %s does not exist!", r.parent, r.name)
 	}
-	return r.meta.releases[r.parent],nil
+	return r.meta.releases[r.parent]
 }
 
 type FlatBuild struct {
-	name,parent string
-	release *FlatRelease
-	barclamps BarclampMap
+	name, parent string
+	release      *FlatRelease
+	barclamps    BarclampMap
 }
 
-func (b *FlatBuild) path() (string) {
-	return filepath.Join(b.release.path(),b.name)
+func (b *FlatBuild) path() string {
+	return filepath.Join(b.release.path(), b.name)
 }
 
-func (b *FlatBuild) Name() (string) {
+// The basic name of a build.
+func (b *FlatBuild) Name() string {
 	return b.name
 }
 
-func (b *FlatBuild) Release() (Release,error) {
-	if b.release == nil {
-		return nil,fmt.Errorf("Build %s is not a member of any release!",b.name)
-	}
-	return b.release,nil
+// The full name of a build.
+// Equal to release.Name() + / + build.Name()
+func (b *FlatBuild) FullName() string {
+	return b.release.Name() + "/" + b.name
 }
 
-func (b *FlatBuild) Parent() (Build,error) {
+// The release that this build is a part of.
+func (b *FlatBuild) Release() Release {
+	if b.release == nil {
+		log.Panicf("Build %s is not a member of any release!", b.name)
+	}
+	return b.release
+}
+
+// The parent build of this one.
+func (b *FlatBuild) Parent() Build {
 	if b.parent == "" {
-		return nil, nil
+		return nil
 	}
 	if b.release.builds[b.parent] == nil {
-		return nil,fmt.Errorf("Release %s: cannot find parent build %s of build %s!",
-			b.release.name,b.parent,b.name)
+		log.Panicf("Release %s: cannot find parent build %s of build %s!",
+			b.release.name, b.parent, b.name)
 	}
-	return b.release.builds[b.parent],nil
+	return b.release.builds[b.parent]
 }
 
-func (b *FlatBuild) Barclamps() (BarclampMap,error) {
-	return b.barclamps,nil
+// The barclamps that are a part of this build.
+func (b *FlatBuild) Barclamps() BarclampMap {
+	return b.barclamps
 }
 
-
-// Get a list of releases in Crowbar
-func (m *FlatMetadata) Releases() (ReleaseMap, error) {
+// Get a list of releases that this metadata knows about
+func (m *FlatMetadata) Releases() ReleaseMap {
 	if m.releases == nil {
-		return nil,fmt.Errorf("No releases available")
+		log.Panicf("No releases available")
 	}
-	return m.releases,nil
+	return m.releases
 }
 
-func (m *FlatMetadata) populateBuild(release *FlatRelease, name string) (Build, error) {
+// Get a list of all the builds this metadata knows about.
+// The returned BuildMap will have build.FullName() keys.
+func (m *FlatMetadata) AllBuilds() BuildMap {
+	res := make(BuildMap)
+	for _, rel := range m.Releases() {
+		for _, bld := range rel.Builds() {
+			res[bld.FullName()] = bld
+		}
+	}
+	return res
+}
+
+func (m *FlatMetadata) populateBuild(release *FlatRelease, name string) Build {
 	build := &FlatBuild{
-		name: name,
-		release: release,
+		name:      name,
+		release:   release,
 		barclamps: make(BarclampMap),
 	}
 	bld := build.path()
-	glob := filepath.Join(bld,"barclamp-*")
-	barclamps,err := filepath.Glob(glob)
-	c := release.meta.crowbar
-	if err != nil {
-		return nil,err
+	parentLink := filepath.Join(bld, "parent")
+	stat, err := os.Lstat(parentLink)
+	if err == nil && (stat.Mode()&os.ModeSymlink) != 0 {
+		parent, err := os.Readlink(parentLink)
+		if err == nil {
+			build.parent = filepath.Base(parent)
+		}
 	}
-	for _,bc := range barclamps {
+	glob := filepath.Join(bld, "barclamp-*")
+	barclamps, err := filepath.Glob(glob)
+	c := release.meta.crowbar
+	dieIfError(err)
+	for _, bc := range barclamps {
 		barclamp := &Barclamp{}
-		barclamp.Name = strings.TrimPrefix(bc,filepath.Join(bld,"barclamp-"))
+		barclamp.Name = strings.TrimPrefix(bc, filepath.Join(bld, "barclamp-"))
 		if c.Barclamps[barclamp.Name] == nil {
-			return nil, fmt.Errorf("Build %s/%s wants %s, which is not in %s\n",
+			log.Panicf("Build %s/%s wants %s, which is not in %s\n",
 				release.name,
 				build.name,
 				barclamp.Name,
 				filepath.Join(c.Repo.Path(),
-				"barclamps",
-				barclamp.Name))
+					"barclamps",
+					barclamp.Name))
 		}
 		barclamp.Repo = c.Barclamps[barclamp.Name]
-		branch,err := ioutil.ReadFile(bc)
+		branch, err := ioutil.ReadFile(bc)
 		if err != nil {
 			continue
 		}
 		buf := bytes.NewBuffer(branch)
 		barclamp.Branch = strings.TrimSpace(buf.String())
-		build.barclamps[barclamp.Name]=barclamp
+		build.barclamps[barclamp.Name] = barclamp
 	}
-	return build,nil
+	return build
 }
 
-func (m *FlatMetadata) populateRelease(rel string) (*FlatRelease, error) {
+func (m *FlatMetadata) populateRelease(rel string) *FlatRelease {
 	release := &FlatRelease{
-		meta: m,
-		name: rel,
+		meta:   m,
+		name:   rel,
 		builds: make(BuildMap),
 	}
 	prefix := release.path()
-	glob := filepath.Join(prefix,"*/")
-	builds,err := filepath.Glob(glob)
-	if err != nil {
-		return nil,err
-	}
-	for _,bld := range builds {
-		bld = strings.Trim(strings.TrimPrefix(bld,prefix),"/")
-		build, err := m.populateBuild(release,bld)
-		if err != nil {
-			return nil,err
+	glob := filepath.Join(prefix, "*/")
+	parentFile := filepath.Join(prefix, "parent")
+	stat, err := os.Stat(parentFile)
+	if err == nil && stat.Mode().IsRegular() {
+		parent, err := ioutil.ReadFile(parentFile)
+		if err == nil {
+			buf := bytes.NewBuffer(parent)
+			release.parent = strings.TrimSpace(buf.String())
 		}
-		release.builds[bld]=build
 	}
-	return release,nil
+	builds, err := filepath.Glob(glob)
+	dieIfError(err)
+	for _, bld := range builds {
+		bld = strings.Trim(strings.TrimPrefix(bld, prefix), "/")
+		build := m.populateBuild(release, bld)
+		dieIfError(err)
+		release.builds[bld] = build
+	}
+	return release
 }
 
-// Populate the Releases field of a Crowbar struct
+// Populate the Releases field of a Crowbar struct, if we are using flat metadata.
 func (m *FlatMetadata) Probe(c *Crowbar) (err error) {
-	m.path = filepath.Join(m.crowbar.Repo.Path(), "releases")
+	m.path = filepath.Join(c.Repo.Path(), "releases")
 	m.crowbar = c
-	stat,err := os.Lstat(m.path)
+	m.releases = make(ReleaseMap)
+	stat, err := os.Lstat(m.path)
 	if err != nil {
-		return fmt.Errorf("Cannot find %s, metadata cannot be flat.",m.path)
+		return fmt.Errorf("Cannot find %s, metadata cannot be flat.", m.path)
 	}
 	if !stat.IsDir() {
-		return fmt.Errorf("%s is not a directory, metadata cannot be flat.",m.path)
+		return fmt.Errorf("%s is not a directory, metadata cannot be flat.", m.path)
 	}
-	stat,err = os.Stat(filepath.Join(m.path,".git"))
+	stat, err = os.Stat(filepath.Join(m.path, ".git"))
 	if err == nil {
-		return fmt.Errorf("%s has a .git directory, metadata cannot be flat.",m.path)
+		return fmt.Errorf("%s has a .git directory, metadata cannot be flat.", m.path)
 	}
 	for _, g := range [...]string{"*/", "feature/*/"} {
-		glob := filepath.Join(m.path,g)
-		releases,err := filepath.Glob(glob)
-		if err != nil {
-			return err
-		}
-		for _,rel := range releases {
-			rel = strings.Trim(strings.TrimPrefix(rel,m.path),"/")
-			release,err := m.populateRelease(rel)
-			if err != nil {
-				return err
-			}
-			m.releases[rel]=release
+		glob := filepath.Join(m.path, g)
+		releases, err := filepath.Glob(glob)
+		dieIfError(err)
+		for _, rel := range releases {
+			rel = strings.Trim(strings.TrimPrefix(rel, m.path), "/")
+			m.releases[rel] = m.populateRelease(rel)
 		}
 	}
 	return nil
